cd34fa6ee897c78afa5e7a4b1ceb64381876a855
J Pratt <jp10010101010000@gmail.com>
2022-01-06 11:53:23 +1100

    Split main into a binary and library
    Change-Id: I758d1ecef2aa057291d28849fe5a9d5867b5a8a8

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-use crepe::crepe;
-use paste::paste;
 use std::collections::HashMap;
 
 // type Ent = &'static str;
 type EntId = u32;
 
 #[derive(Copy, Clone, PartialOrd, Ord, Eq, PartialEq, Hash)]
-struct Ent {
+pub struct Ent {
     id: EntId,
 }
 
@@ -34,7 +32,7 @@ fn name_by_id() -> &'static mut HashMap<EntId, String> {
 }
 
 impl Ent {
-    fn new(name: &str) -> Self {
+    pub fn new(name: &str) -> Self {
         let id = unsafe {
             LAST_ENT += 1;
             LAST_ENT
@@ -44,17 +42,17 @@ impl Ent {
         Ent { id }
     }
 
-    fn name(&self) -> &String {
+    pub fn name(&self) -> &String {
         name_by_id()
             .get(&self.id)
             .expect("All entities should have a name")
     }
 
-    fn get_by_name(name: &str) -> Option<Ent> {
+    pub fn get_by_name(name: &str) -> Option<Ent> {
         id_by_name().get(name).map(|id| Ent { id: *id })
     }
 
-    fn by_name(name: &str) -> Ent {
+    pub fn by_name(name: &str) -> Ent {
         Ent::get_by_name(name).unwrap_or_else(|| Ent::new(name))
     }
 }
@@ -74,8 +72,10 @@ impl std::fmt::Debug for Ent {
     }
 }
 
+#[macro_export]
 macro_rules! relation {
     ($name: ident $(, $arg: expr )* ) => {
+        use paste::paste;
         @input
         struct paste!([<$name Claim>])( $($arg, )* )
         @output
@@ -85,9 +85,11 @@ macro_rules! relation {
     }
 }
 
+#[macro_export]
 macro_rules! facts {
     ($runtime: expr $(, $name: ident ($first: expr $(, $arg: expr )*) )+ ) => {
         {
+            use paste::paste;
             $(
                 $runtime.extend(&[
                     paste!( [< $name Claim >]) (
@@ -97,76 +99,4 @@ macro_rules! facts {
             )*
         }
     };
-}
-
-crepe! {
-    struct Exists(Ent);
-
-    @input
-    struct SubtypeClaim(Ent, Ent);
-    @output
-    struct Subtype(Ent, Ent);
-    Subtype(x,y) <- SubtypeClaim(x, y);
-    // relation!{Subtype(Ent, Ent)}
-
-    @input
-    struct HasTagClaim(Ent, Ent);
-    @output
-    struct HasTag(Ent, Ent);
-    HasTag(x,y) <- HasTagClaim(x, y);
-
-    Exists(x) <- Subtype(x, _);
-    Exists(x) <- Subtype(_, x);
-
-    @output
-    struct Man(Ent);
-    Man(x) <- Subtype(x, Ent::by_name("man")), Subtype(x, Ent::by_name("individual"));
-
-    Man(Ent::by_name(&("Mr. ".to_string()+y.name()))) <- Man(y), (&y.name()[0..4] != "Mr. ");
-    // Subtype("man", "mortal") <- (true);
-
-    Subtype(x,x) <- Subtype(x, _);
-    Subtype(x,x) <- Subtype(_, x);
-    Subtype(x, z) <- Subtype(x, y), Subtype(y, z);
-    HasTag(x, z) <- Subtype(x, y), HasTag(y, z);
-}
-
-fn main() {
-    let mut runtime = Crepe::new();
-
-    let plato = Ent::by_name("plato");
-    let individual = Ent::by_name("individual");
-    let socretes = Ent::by_name("socretes");
-    let man = Ent::by_name("man");
-    let mortal = Ent::by_name("mortal");
-
-    // specify all the 'dynamic' facts
-    facts!(
-        runtime,
-        Subtype(plato, individual),
-        Subtype(socretes, individual),
-        Subtype(plato, man),
-        Subtype(socretes, man),
-        HasTag(man, mortal)
-    );
-
-    let (subtypes, tags, men) = &runtime.run();
-    for Subtype(x, y) in subtypes {
-        //if *x != "socretes" {
-        //continue;
-        //}
-        println!("{} is a {}", x, y);
-    }
-    for HasTag(x, y) in tags {
-        //if *x != "socretes" {
-        //continue;
-        //}
-        println!("{} has tag {}", x, y);
-    }
-    for Man(x) in men {
-        //if *x != "socretes" {
-        //continue;
-        //}
-        println!("{} 'is a man'", x);
-    }
 }
