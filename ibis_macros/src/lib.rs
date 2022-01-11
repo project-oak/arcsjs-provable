6bddf617da21cd6e4ce0287a585c1a41c0ee2a7f
J Pratt <jp10010101010000@gmail.com>
2022-01-12 09:40:44 +1100

    Better argument counting
    Change-Id: I1edd6716eb5bf5ca1063de9ca7bfcc62771c5bf0

--- ibis_macros/src/lib.rs
+++ ibis_macros/src/lib.rs
@@ -12,6 +12,7 @@ pub fn ibis(input: TokenStream) -> TokenStream {
         if definition.is_empty() {
             return;
         }
+
         definition.reverse();
         let name = definition.pop().expect("Definition must have a name");
         if definition.is_empty() {
@@ -26,22 +27,43 @@ pub fn ibis(input: TokenStream) -> TokenStream {
             let mut arity = 0;
             let mut arg_names: Vec<String> = vec![];
 
+            let mut curr_arg = false; // we're not currently in an arg
+            let mut make_arg = || {
+                arity += 1;
+                arg_names.push(format!("arg{}", arity));
+            };
+
             match &args {
                 Group (stream) => {
                     for token in stream.stream() {
                         match token {
-                            Punct(_) => {},
-                            _arg => {
-                                arity += 1;
-                                arg_names.push(format!("arg{}", arity));
-                            }
+                            Punct(ch) => {
+                                if ch == ',' {
+                                    make_arg();
+                                    curr_arg = false; // we're not currently in an arg
+                                    continue;
+                                }
+                            },
+                            _arg => {}
                         }
+                        curr_arg = true;
                     }
                 },
                 _ => {
                     panic!("expected {} to be a group", args);
                 }
             }
+            if curr_arg {
+                // finish the arg
+                make_arg();
+            }
+
+            let name = format!("{}", name);
+            let claim_name = if name.ends_with("Claim") {
+                format!("{}Claim", &name)
+            } else {
+                name.clone()
+            };
 
             // this is a struct definition
             definitions += &format!("
