2a1201dfd5c59fd6ec9ee3fb8bccc21b7ce67f60
J Pratt <jp10010101010000@gmail.com>
2022-02-07 14:33:47 +1100

    Add BSD headers
    Change-Id: Iada0043a04db0cdf4ffb62fd8ff4d86e80095881

--- ibis/watch.sh
+++ ibis/watch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright 2022 Google LLC
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
 _term() {
   echo "Caught SIGTERM signal: killing watchers"
   kill -TERM "$WATCH_PID" 2>/dev/null
