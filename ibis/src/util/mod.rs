7b5475bcf87b095fd2e13fa5ec7bf79de5a77ca3
J Pratt <jp10010101010000@gmail.com>
2022-02-15 15:47:02 +1100

    Clean up util module
    Change-Id: I9c9e3a59be23eeaf1b62f01efb78ed730c1a4312

---
+++ ibis/src/util/mod.rs
@@ -0,0 +1,48 @@
+// Copyright 2022 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+mod bimap;
+pub use bimap::*;
+
+#[macro_export]
+macro_rules! map {
+    () => {
+        std::collections::BTreeMap::new()
+    };
+    ( $( $key: expr => $value: expr ),* $(,)?) => {
+        {
+            let mut st = map!();
+            $(
+                st.insert( $arg, $value );
+            )*
+            st
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! set {
+    () => {
+        std::collections::BTreeSet::new()
+    };
+    ( $( $arg: expr ),* $(,)?) => {
+        {
+            let mut st = set!();
+            $(
+                st.insert( $arg );
+            )*
+            st
+        }
+    };
+}
+
+pub struct Raw(pub String);
+
+impl std::fmt::Debug for Raw {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
