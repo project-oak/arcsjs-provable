1f59ba2120aa2872ceb8e1d340d5aa1f71333293
J Pratt <jp10010101010000@gmail.com>
2022-02-18 14:23:58 +1100

    Update tests to use renamed get_solutions
    Change-Id: I71e71fcccd55da17b0be9e1d15098de67a145385

--- ibis/tests/utils.rs
+++ ibis/tests/utils.rs
@@ -4,14 +4,14 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-use ibis::{solutions, Ibis, Recipe};
+use ibis::{get_solutions, Ibis, Recipe};
 
 fn map_solutions_with_edge_loss<U>(
     data: &str,
     loss: Option<usize>,
     on_result: &dyn Fn(&Recipe) -> U,
 ) -> Vec<U> {
-    let recipies: Ibis = solutions(data, loss);
+    let recipies: Ibis = get_solutions(data, loss);
     recipies
         .recipies
         .iter()
