241172124fd76a33cdb58ece2a305665f9733b75
J Pratt <jp10010101010000@gmail.com>
2022-03-18 12:22:51 +1100

    Add support for multiple files and multiple outputs (#63)
    * Add support for handling multiple files
    * Remove unused css and widen the input file frame
    * Add a 'clear output' button
    * Start writing a wrapper for ibis for ArcsJs
    * Catch, report and rethrow exceptions for client code to respond to
    * Re-render dot output on tab switch

---
+++ ibis/playground/file-pane.js
@@ -0,0 +1,153 @@
+// Copyright 2022 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+const template = `
+  <style>
+    #content {
+      font-size: 12px;
+      min-width: 700px;
+      padding: 10px;
+      border: 1px solid #ccc;
+    }
+    .tab-panel {
+      width: fit-content;
+      margin: 10px 0 20px 0;
+    }
+    .tab-row {
+      overflow: hidden;
+      background-color: #f1f1f1;
+      border: 1px solid #ccc;
+      border-bottom: none;
+    }
+    .tab-row button {
+      background-color: inherit;
+      cursor: pointer;
+      float: left;
+      border: none;
+      outline: none;
+      padding: 10px 16px;
+    }
+    .tab-row button:hover {
+      background-color: #ddd;
+    }
+    .tab-row button.active {
+      background-color: #ccc;
+    }
+    #files textarea {
+      font-size: 12px;
+      min-width: 700px;
+      width: 90vw;
+      padding: 10px;
+      border: 1px solid #ccc;
+    }
+  </style>
+  <div class="tab-panel">
+    <div class="tab-row">
+        <div id="tabs"></div>
+        <button id="add-button">+</button>
+    </div>
+    <div id="files"></div>
+    </div>
+  </div>`;
+
+export class FilePane extends HTMLElement {
+
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.innerHTML = template;
+
+        this.tabs = shadowRoot.getElementById('tabs');
+        this.files = shadowRoot.getElementById('files');
+        this.addButton = shadowRoot.getElementById('add-button');
+
+        this.files.addEventListener('keypress', this.interceptCtrlEnter.bind(this));
+        this.addButton.addEventListener('click', this.addFile.bind(this));
+        this.fileBase = 'a'.charCodeAt(0);
+    }
+
+    connectedCallback() {
+    }
+
+    addExecuteCallback(executeCallback) {
+        this.executeCallback = executeCallback;
+    }
+
+    addTabSwitchCallback(tabSwitchCallback) {
+        this.tabSwitchCallback = tabSwitchCallback;
+    }
+
+    interceptCtrlEnter(event) {
+        if (event.key === 'Enter' && event.ctrlKey) {
+            if (this.executeCallback) {
+                this.executeCallback();
+            }
+            event.preventDefault();
+        }
+    }
+
+    static get observedAttributes() {
+        return ['no-add-button'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(this, name, oldValue, newValue);
+        if (name === 'no-add-button') {
+            if (newValue) {
+                this.addButton.style.display = 'none';
+            } else {
+                this.addButton.style.display = '';
+            }
+        }
+    }
+
+    addFile(event, content) {
+        const file = document.createElement('textarea');
+        file.rows = 10;
+        file.spellcheck = false;
+        file.addEventListener('keypress', this.interceptCtrlEnter.bind(this))
+        file.value = content || '';
+
+        const tab = document.createElement('button');
+        tab.textContent = `${String.fromCharCode(this.fileBase++)}`;
+        tab.linkedFile = file;
+        tab.addEventListener('click', this.showFile.bind(this))
+
+        this.tabs.appendChild(tab);
+        this.files.appendChild(file);
+        tab.click();
+
+        if (this.fileBase > 'z'.charCodeAt(0)) {
+            this.addButton.style.display = 'none';
+        }
+        return file;
+    }
+
+    dropAllFiles() {
+        this.files.replaceChildren();
+        this.tabs.replaceChildren();
+        this.fileBase = 'a'.charCodeAt(0);
+    }
+
+    getFileContents() {
+        return Array.from(this.files.children).map(file => file.value);
+    }
+
+    showFile(event) {
+        for (const tab of this.tabs.children) {
+            tab.classList.remove('active');
+        }
+        for (const file of this.files.children) {
+            file.style.display = 'none';
+        }
+        event.target.classList.add('active');
+        event.target.linkedFile.style.display = '';
+        this.active = event.target.linkedFile;
+        if (this.tabSwitchCallback) {
+            this.tabSwitchCallback();
+        }
+    }
+}
--- ibis/playground/playground.js
+++ ibis/playground/playground.js
@@ -1,6 +1,14 @@
+// Copyright 2022 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
 import './third_party/viz.js';
 import './third_party/full.render.js';
-import {default as ibis, version_info, best_solutions_to_json, best_solutions_to_dot} from '../pkg/ibis.js';
+import {loadIbis, best_solutions_to_json, best_solutions_to_dot} from '../ibis.js';
+import {FilePane} from './file-pane.js';
+
+window.customElements.define('file-pane', FilePane);
 
 function render(dot) {
   var viz = new Viz();
@@ -19,54 +27,65 @@ function render(dot) {
   });
 }
 
-async function loadIbis() {
-    const feedback = document.getElementById('feedback');
-    const version_info_display = document.getElementById('version_info');
-    feedback.innerText = 'Loading ibis...';
-    // Set up ibis
-    await ibis('../pkg/ibis_bg.wasm');
-    feedback.innerText = 'Loaded';
-    version_info_display.innerText = version_info();
-}
-
 async function getDemoContent() {
     const demo = await fetch('../demo.json');
     const demoText = await demo.text();
-    const input = document.getElementById('input');
-    input.value = demoText;
+    const filePane = document.getElementById('filePane');
+    filePane.addFile(undefined, demoText);
 }
 
 async function startup() {
-    const input = document.getElementById('input');
+    const filePane = document.getElementById('filePane');
+    const outputPane = document.getElementById('outputPane');
     const to_json = document.getElementById('to_json');
+    const clear_output = document.getElementById('clear_output');
     to_json.addEventListener("click", () => run(best_solutions_to_json,
         input => JSON.stringify(JSON.parse(input), undefined, 2)
     ));
-    const to_dot = document.getElementById('to_dot');
-    to_dot.addEventListener("click", () => run(best_solutions_to_dot, dot => {
+
+    const to_dot_callback = () => run(best_solutions_to_dot, dot => {
         render(dot);
         return dot;
-    }));
+    });
+    const to_dot = document.getElementById('to_dot');
+    to_dot.addEventListener("click", to_dot_callback);
+    filePane.addExecuteCallback(to_dot_callback);
 
-    await Promise.all([loadIbis(), getDemoContent()]);
+    outputPane.addTabSwitchCallback(() => {
+        render(outputPane.active.value);
+    });
+
+    clear_output.addEventListener("click", () => {
+        outputPane.dropAllFiles();
+    });
+
+    await Promise.all([
+        loadIbis(
+            '../pkg/ibis_bg.wasm',
+            (status_text, style) => {
+            const feedback = document.getElementById('feedback');
+            feedback.innerText = status_text;
+            if (style === "error") {
+                feedback.classList.add('error');
+            } else {
+                feedback.classList.remove('error');
+            }
+        },
+            (version_info) => {
+                const version_info_display = document.getElementById('version_info');
+                version_info_display.innerText = version_info;
+            }
+        ),
+        getDemoContent()
+    ]);
 }
 
 async function run(ibis_function, formatter) {
-    const input = document.getElementById('input');
-    const output = document.getElementById('output');
-    const feedback = document.getElementById('feedback');
-    feedback.innerText = 'Running...';
-    const inputText = input.value;
-    if (!inputText) {
-        output.value = 'no text';
-        return;
-    }
-    // update it
-    const startTime = performance.now()
-    const result = ibis_function(inputText);
-    const endTime = performance.now()
-    feedback.innerText = `Done in ${(endTime-startTime)/1000.0} seconds`;
-    output.value = formatter(result);
+    const filePane = document.getElementById('filePane');
+    const outputPane = document.getElementById('outputPane');
+    const result = ibis_function(filePane.getFileContents());
+    const outputFile = outputPane.addFile(undefined, formatter(result));
+    outputFile.disabled = true;
 }
 
 window.onload = function() {
