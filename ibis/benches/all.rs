a3cd81b522f4bb93879673b93a171d4f581b5fdc
J Pratt <jp10010101010000@gmail.com>
2022-03-30 14:48:47 +1100

    Create benchmarks for useful idioms that Ibis uses
    Change-Id: Ieaa9ceae117d39f41ca7e44c724d685bc0214347

--- ibis/benches/all.rs
+++ ibis/benches/all.rs
@@ -4,20 +4,27 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-use criterion::{criterion_group, criterion_main};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 mod checking_and_planning;
 mod checking_only;
 mod demo;
+mod utils;
 
 use checking_and_planning::*;
 use checking_only::*;
 use demo::*;
+use utils::*;
 
+criterion_group!(
+    name = micro_benches;
+    config = Criterion::default().sample_size(10000);
+    targets = criterion_benchmark_new_vec_push
+);
 criterion_group!(
     benches,
     criterion_benchmark_checking_only,
     criterion_benchmark_noop_planning,
     criterion_benchmark_solve_demo
 );
-criterion_main!(benches);
+criterion_main!(benches, micro_benches);
