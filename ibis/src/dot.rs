2ca4bb316418f6239ad4d0c44b44f6c506c9dcc8
J Pratt <jp10010101010000@gmail.com>
2022-04-13 20:29:32 +1000

    Add licenses

--- ibis/src/dot.rs
+++ ibis/src/dot.rs
@@ -1,3 +1,8 @@
+// Copyright 2022 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
 #[derive(Default)]
 pub struct DotGraph {
     nodes: Vec<String>,
