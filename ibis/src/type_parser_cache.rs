f2c6954ed08334b8a7ea590542e3f1a734fdddf1
J Pratt <jp10010101010000@gmail.com>
2022-04-13 11:09:56 +1000

    TMP - added

--- ibis/src/type_parser_cache.rs
+++ ibis/src/type_parser_cache.rs
@@ -7,20 +7,21 @@
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::cell::RefCell;
+use std::sync::Arc;
 use std::sync::Mutex;
 use std::borrow::BorrowMut;
 use crate::type_parser::read_type_uncached;
 use crate::type_struct::Type;
 
 lazy_static! {
-    pub static ref PARSE_CACHE: Mutex<RefCell<HashMap<String, Type>>> = Mutex::new(RefCell::new(HashMap::new()));
+    pub static ref PARSE_CACHE: Mutex<RefCell<HashMap<String, Arc<Type>>>> = Mutex::new(RefCell::new(HashMap::new()));
 }
-pub fn read_type(input: &str) -> Type {
+pub fn read_type(input: &str) -> Arc<Type> {
     let guard = PARSE_CACHE.lock().expect("Shouldn't fail");
     let mut ctx = (*guard).borrow_mut();
     ctx.borrow_mut()
         .entry(input.to_string())
-        .or_insert_with(|| read_type_uncached(input))
+        .or_insert_with(|| Arc::new(read_type_uncached(input)))
         .clone()
 }
 
