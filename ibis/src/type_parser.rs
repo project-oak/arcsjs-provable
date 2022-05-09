d1e73bb307b96b637b7d146724b9a6b8cc700abe
J Pratt <jp10010101010000@gmail.com>
2022-05-09 12:27:17 +1000

    Store args as Arcs
    Change-Id: I46fd4b1d9c2c833891a3686147308bc0b7664725

--- ibis/src/type_parser.rs
+++ ibis/src/type_parser.rs
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
-
 extern crate nom;
 use crate::type_struct::*;
 use nom::{
