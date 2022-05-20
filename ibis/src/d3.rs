0385f0516478724b1258170660491ae8143328b8
J Pratt <jp10010101010000@gmail.com>
2022-05-20 14:26:54 -0700

    TMP - added

--- ibis/src/d3.rs
+++ ibis/src/d3.rs
@@ -3,14 +3,31 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
-use crate::recipes::{Ibis, Recipe};
+use crate::ent::EntityIdBackingType;
+use crate::recipes::{Ibis, Node, Recipe};
+use crate::{Ent, Sol};
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+
+#[derive(Default, Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde()]
+pub struct D3Node {
+    id: EntityIdBackingType,
+    name: String,
+}
+
+#[derive(Default, Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde()]
+pub struct Link {
+    source: EntityIdBackingType,
+    target: EntityIdBackingType,
+}
 
 #[derive(Default, Debug, Serialize, Deserialize, Eq, PartialEq)]
 #[serde()]
 pub struct D3Graph {
-    nodes: Vec<String>,
-    edges: Vec<(String, String, Vec<String>)>,
+    nodes: Vec<D3Node>,
+    links: Vec<Link>,
 }
 
 pub trait ToD3 {
@@ -18,23 +35,60 @@ pub trait ToD3 {
 }
 
 impl D3Graph {
-    pub fn add_node(&mut self, node: String) {
+    pub fn add_node(&mut self, node: D3Node) {
         self.nodes.push(node);
     }
 
-    pub fn add_edge(&mut self, from: String, to: String, attrs: Vec<String>) {
-        self.edges.push((from, to, attrs));
+    pub fn add_link(&mut self, link: Link) {
+        self.links.push(link);
+    }
+
+    pub fn merge(&mut self, other: Self) {
+        self.nodes.extend(other.nodes);
+        self.links.extend(other.links);
     }
 }
 
-impl ToD3 for Recipe {
+impl ToD3 for (&Ibis, &Recipe) {
     fn to_d3(&self) -> D3Graph {
-        D3Graph::default()
+        let (ibis, recipe) = &self;
+        let mut d3 = D3Graph::default();
+
+        let mut particles: HashSet<Ent> = HashSet::new();
+
+        for Node ( particle, node, ty ) in &ibis.shared.nodes {
+            particles.insert(*particle);
+            d3.add_node(D3Node {
+                id: node.id,
+                name: format!("{}: {}", node, ty),
+            });
+        }
+        for particle in particles {
+            d3.add_node(D3Node {
+                id: particle.id,
+                name: format!("{}", particle),
+            });
+        }
+        let sol = &recipe.id.unwrap_or_else(Sol::empty).solution();
+        for (source, target) in &sol.edges {
+            d3.add_link(Link {
+                source: source.id,
+                target: target.id,
+            });
+        }
+
+        d3
     }
 }
 
 impl ToD3 for Ibis {
     fn to_d3(&self) -> D3Graph {
-        D3Graph::default()
+        let mut d3 = D3Graph::default();
+
+        for recipe in &self.recipes {
+            let recipe_d3 = (self, recipe).to_d3();
+            d3.merge(recipe_d3);
+        }
+        d3
     }
 }
---
+++ ibis/todo.md
@@ -0,0 +1,14 @@
+COPY
+
+policy renderer -> For the output
+
+data-explorer for the rendering
+
+
+FontChooser
+
+-> app/App.js
+
+loadIbis
+
+window.App.resolvedPolicy
